38e972fcffc40cb03dc6e0b0ef21ed104dc020cc
AwaisQarni <awaisqarni126@gmail.com>
2020-10-08 16:41:50 +0500

    odoo module

--- ODOOPROJECT/my_addons/openacademy/__manifest__.py
+++ ODOOPROJECT/my_addons/openacademy/__manifest__.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Open Academy",
+    'name': "openacademy",
 
-    'summary': """Manage trainings""",
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
 
     'description': """
-        Open Academy module for managing trainings:
-            - training courses
-            - training sessions
-            - attendees registration
+        Long description of module's purpose
     """,
 
     'author': "My Company",
@@ -17,7 +16,7 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Test',
+    'category': 'Uncategorized',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
@@ -25,14 +24,13 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'templates.xml',
+        'security/ir.model.access.csv',
+        'views/openacademy.xml',
+        #'views/templates.xml',
+        'security/security.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
-        'demo.xml',
+        'demo/demo.xml',
     ],
-    'installable': True,
-    'application': True,
-    'auto_install': False,
 }
--- ODOOPROJECT/my_addons/openacademy/controllers/controllers.py
+++ ODOOPROJECT/my_addons/openacademy/controllers/controllers.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-from odoo import http
+# from odoo import http
+
 
 # class Openacademy(http.Controller):
 #     @http.route('/openacademy/openacademy/', auth='public')
